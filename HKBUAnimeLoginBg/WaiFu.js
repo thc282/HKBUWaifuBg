09ce8bae938ce64311d9569379da4a61e708224e
thc282 <thc282@gmail.com>
2023-11-03 00:36:07 +0800

    1. Add Loading GIF when photo not ready 2. Adjust the Login wrapper opacity 3. Using Base64 to set as css background

--- HKBUAnimeLoginBg/WaiFu.js
+++ HKBUAnimeLoginBg/WaiFu.js
@@ -1,5 +1,7 @@
-//https://konachan.net/post.json?limit=5&tags=width:1920 height:1080 order:random
-let PhotoURL
+//https://konachan.net/post.json?limit=2&tags=width:1920 height:1080 order:randomlet PhotoURL
+var Bgpic = undefined
+let LoadingGIF = chrome.runtime.getURL('windows-xp-loading-screen.gif')
+
 CheckDOM()
 FetchPic()
 
@@ -15,10 +17,15 @@ function FetchPic(){
         PhotoURL = response.jpg
         /*then(res=>window.URL.createObjectURL(new Blob([res])))
         let b = response.blob*/
+        PhotoURL = response.base64
         CheckDOM("ReplaceImg")
+        console.log(`data to base64 :\n${response.base64}`)
         console.log(response.status)
         console.log(response.jpg)
+        console.log(`Photo source: ${response.jpg}`)
+        console.log("Search result: ")
         console.log(response.json)
+        console.log("If Error: " + response.err)
     })
 }
     
@@ -40,11 +47,63 @@ function afterDOMLoaded(action){
             break;
         default:
             console.log("DOMloaded")
+            RemoveOriBg()
+            AdjustLoginWrapper()
             break
     }
 }
 
 function ReplaceImg(){
-    var Bgpic = document.getElementById("body_sign_in")
-    Bgpic.style.backgroundImage = `url('${PhotoURL}')`
+    Bgpic.style.backgroundImage = `url(${PhotoURL})`
+}
+
+function RemoveOriBg(element){
+    if(element !== undefined){
+        Bgpic = element
+        Bgpic.style.backgroundImage = `url(${LoadingGIF})`;
+        console.log("Original img set to Loading Img....")
+    }else{
+        console.log("Waiting Original img bg")
+        waitForElementToExist("#body_sign_in")
+    }
+}
+
+function AdjustLoginWrapper(element){
+    if(element !== undefined){
+        //The wrapper element
+        element.style.backgroundColor = "rgba(255,255,255,0.75)"
+        //Lower part of wrapper
+        let low = document.querySelector('[class="center-align"]')
+        low.style.setProperty("background-color", 'initial')
+        console.log("Wrapper Adjusted")
+    }else{
+        console.log("Waiting Wrapper")
+        waitForElementToExist('#content')
+    }
+}
+
+function waitForElementToExist(selector) {
+    if (document.querySelector(selector)) {
+      console.log(`${selector} elem exists`);
+      switch(selector){
+        case '#content':
+            return AdjustLoginWrapper(document.querySelector(selector));
+        case "#body_sign_in":
+            return RemoveOriBg(document.querySelector(selector));
+      }
+      
+    }
+  
+    const intervalID = setInterval(() => {
+      if (document.querySelector(selector)) {
+        console.log(`${selector} elem exists`);
+        clearInterval(intervalID);
+        switch(selector){
+            case '#content':
+                return AdjustLoginWrapper(document.querySelector(selector));
+            case "#body_sign_in":
+                return RemoveOriBg(document.querySelector(selector));
+          }
+      }
+    }, 100);
 }
